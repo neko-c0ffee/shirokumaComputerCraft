00dbd211b450b5cec27e5b547d99283c1a2bf958
neko-c0ffee <mika021195@gmail.com>
2024-09-30 15:28:51 +0000

    diskにバックアップするプログラムの作成

---
+++ src/backupToDisk.lua
@@ -0,0 +1,8 @@
+local fileList = fs.list('')
+for key, fileName in pairs(fileList) do
+  if fileName ~= 'disk' and fileName ~= 'rom' then
+    local destination = '/disk/'..fileName
+    fs.delete(destination)
+    fs.copy(fileName, destination)
+  end
+end
